1ebf617e236d598075b5412ac28f01e03a4a6566
GreyEntity <83161732+GreyEntity@users.noreply.github.com>
2021-12-11 09:54:19 +0700

    Add files via upload

---
+++ Victorium 1.8/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Victorium1.8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Victorium1.8")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7f467f02-4e47-49ed-9261-bf593e83e7c4")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.8.0.0")]
