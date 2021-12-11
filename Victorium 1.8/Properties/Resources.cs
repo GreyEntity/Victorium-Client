38398ad0033397eec265b689aa8253ed4feb97cc
GreyEntity <83161732+GreyEntity@users.noreply.github.com>
2021-12-11 09:55:18 +0700

    Add files via upload

---
+++ Victorium 1.8/Properties/Resources.cs
@@ -0,0 +1,40 @@
+﻿using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace Victorium1p8.Properties
+{
+    [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [DebuggerNonUserCode]
+    [CompilerGenerated]
+    internal class Resources
+    {
+        private static ResourceManager resourceMan;
+        private static CultureInfo resourceCulture;
+
+        internal Resources()
+        {
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static ResourceManager ResourceManager
+        {
+            get
+            {
+                if (Victorium1p8.Properties.Resources.resourceMan == null)
+                    Victorium1p8.Properties.Resources.resourceMan = new ResourceManager("Victorium1p8.Properties.Resources", typeof(Victorium1p8.Properties.Resources).Assembly);
+                return Victorium1p8.Properties.Resources.resourceMan;
+            }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static CultureInfo Culture
+        {
+            get => Victorium1p8.Properties.Resources.resourceCulture;
+            set => Victorium1p8.Properties.Resources.resourceCulture = value;
+        }
+    }
+}
