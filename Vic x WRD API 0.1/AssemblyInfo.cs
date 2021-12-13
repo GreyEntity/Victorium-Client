8b04af00227529dfb739f2065f618f0dab26166e
GreyEntity <83161732+GreyEntity@users.noreply.github.com>
2021-12-13 13:05:59 +0700

    Add files via upload

---
+++ Vic x WRD API 0.1/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeAreDevs API x Victorium")]
+[assembly: AssemblyDescription("Used to build software accessing the Lua C API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeAreDevs_API.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6d8a8bf5-f458-403a-a65f-8989c36536fd")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
